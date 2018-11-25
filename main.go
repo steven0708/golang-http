5f4c1de23bf64a3683c212e79f5dc4e919eece22
steven0708 <onek0708@gmail.com>
2018-11-25 14:29:20 +0900

    add try

--- main.go
+++ main.go
@@ -7,7 +7,7 @@ import (
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
-	title := "Jenkins X golang http example Demo! Change Greeing Message"
+	title := "Jenkins X golang http example Demo! Change Greeing Message try 2"
 
 	from := ""
 	if r.URL != nil {
